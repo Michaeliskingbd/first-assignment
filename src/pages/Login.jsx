b5203d05443f18a2f41bc2a493e4e3676bf06e27
Michael Aladejuyigbe <michaeliskingbd@gmail.com>
2025-05-02 15:53:16 +0100

    forth commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isChecked, setIsChecked] = useState(false);
+  const [error, setError] = useState(false);
+  const [sucessMessage, setSuccessMessage] = useState(false);
+  const [showpassword, setShowPassword] = useState(false);
+
+  const togglePassword = () => {
+    setShowPassword(!showpassword);
+  };
+
+  const Verify = (e) => {
+    e.preventDefault();
+    if (email && password && isChecked) {
+      console.log(email, password, isChecked);
+      setSuccessMessage(true);
+    } else {
+      setError(true);
+    }
+  };
+
   return (
     <div className="min-h-screen bg-gradient-to-r from-blue-500 to-purple-600 flex items-center justify-center">
       <div className="bg-white p-8 rounded-2xl shadow-2xl w-full max-w-md">
         <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">
           Login
         </h2>
-        <form className="space-y-4">
+        <form onSubmit={Verify} className="space-y-4">
           <div>
             <label className="block text-sm font-medium text-gray-700">
               Email
             </label>
             <input
               type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               placeholder="Enter your email"
               className="w-full px-4 py-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
@@ -23,11 +46,26 @@ const Login = () => {
               Password
             </label>
             <input
-              type="password"
+              type={showpassword ? "text" : "password"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               placeholder="Enter your password"
               className="w-full px-4 py-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
+          <input
+            type="checkbox"
+            checked={isChecked}
+            onChange={(e) => setIsChecked(e.target.value)}
+          />
+
+          {error && (
+            <p className="text-red-600">Please fill in all credentials</p>
+          )}
+          {sucessMessage && (
+            <p className="text-green-600">Logged in successfully</p>
+          )}
+
           <button
             type="submit"
             className="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg transition duration-200"
